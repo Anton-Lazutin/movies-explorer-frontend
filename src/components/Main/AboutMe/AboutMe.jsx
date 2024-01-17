b9afe03a32f23579d161e267161345cb2033df67
Anton Lazutin <laa.63@yandex.ru>
2024-01-17 15:41:30 +0300

    30yo

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
       <div className="about-me__container">
         <div className="about-me__text-container">
           <h3 className="about-me__name">Антон</h3>
-          <p className="about-me__job">Фронтенд-разработчик, 29 лет</p>
+          <p className="about-me__job">Фронтенд-разработчик, 30 лет</p>
           <p className="about-me__description">
             Я родился в прекрасном городе Самара, но в 17 лет переехал в
             Санкт-Петербург. Увлекаюсь горными лыжами, а так же люблю
